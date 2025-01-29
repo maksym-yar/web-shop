e7802eb153a00a727d8fbccde28be254d69b1136
maksym-yar <maksymyaremenko3@gmail.com>
2025-01-29 22:18:14 +0200

    fixed build

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -18,6 +18,7 @@ const authOptions: NextAuthOptions = {
     }),
   ],
 }
-export const handler =  NextAuth(authOptions)
+
+const handler =  NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
